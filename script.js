a8c2b89a9292631e68a241e7ffdab377ad44c2e9
Maggie Walker <mags21walker@gmail.com>
2019-03-14 23:00:35 -0400

    Changed logical flow. Event listener action not consistent

--- script.js
+++ script.js
@@ -1,53 +1,66 @@
 let listOfDollars = document.getElementsByClassName('dollar');
 let dollarsArr = [...listOfDollars]
 let gameboard = document.getElementById('gameBoard');
-let testDollar = document.getElementById('test')
 
 //Helper Functions
-let chooseRandom = function(domElemArr, func) {
+let deleteDom = function(parent, child){
+    parent.removeChild(child);
+    dollarsArr = [...document.getElementsByClassName('dollar')];
+    console.log('list after delete', dollarsArr)
+}
+
+
+let chooseRandom = function(domElemArr) {
     let len = domElemArr.length
     let chosen = domElemArr[Math.floor(Math.random() * len)]
-    chosen.addEventListener('click', function() {
-        func();
+
+    // Give chosen box chosen id
+    chosen.id = 'chosen';
+}
+
+//Make all dollars id = notchosen
+dollarsArr.forEach(dollar => {
+    dollar.id = 'notchosen'
+    console.log('dollarid', dollar.id)
+})
+
+
+let createBox =  function(func) {
+    //Make all boxes not chosen
+    dollarsArr.forEach(dollar => {
+        dollar.id = 'notchosen'
     })
-    console.log("chosen random box", chosen)
-}
 
-let deleteDom = function(parent, child){
-    return function(){
-        parent.removeChild(child)
-    }
-}
-
-let createBox =  function() {
     ///Create new box
     let newBox = document.createElement('div');
     newBox.style.cssText = `top: ${Math.floor(Math.random() * 680)}px; left: ${Math.floor(Math.random() * 980)}px`;
     newBox.className = 'dollar'
     gameboard.appendChild(newBox)
     console.log('new box', newBox)
-    dollarsArr = [...dollarsArr, newBox]
-    console.log('dollars array', dollarsArr)
+    dollarsArr = [...document.getElementsByClassName('dollar')]
 
+    func(createBox)
 
-    ///Give all boxes delete event listener
-    // dollarsArr.forEach(dollar => {
-    //     dollar.addEventListener('click', function(){
-    //         deleteDom(gameboard, dollar)
-    //     })
-    // }); 
+    ///Give one box createBox functionality
+    chooseRandom(dollarsArr)
+    console.log('dollars array after new', dollarsArr)
 
-    dollarsArr.forEach(dollar => {
-        dollar.addEventListener('click', deleteDom(gameboard, dollar))
-    }); 
-
-
-    ///Give one box createBox event listener
-    chooseRandom(dollarsArr, createBox)
 }
 
-chooseRandom(dollarsArr, createBox);
+///Give all boxes conditional event listener
+let addClick = function () {
+    dollarsArr.forEach(dollar => {
+        dollar.addEventListener('click', function () {
+            if (dollar.id === 'notchosen') {
+                deleteDom(gameboard, dollar)
+            } else if (dollar.id === 'chosen') {
+                createBox(addClick)
+            }
+        })
+    })
+}; 
 
-// testDollar.addEventListener('click', function() {
-//     deleteDom(gameboard, testDollar)
-// })
+//Code Begins
+chooseRandom(dollarsArr);
+addClick();
+
